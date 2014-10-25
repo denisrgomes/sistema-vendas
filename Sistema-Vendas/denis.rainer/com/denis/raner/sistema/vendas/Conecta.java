ca9494b7b4bea5177b64a81e87b31afd40df261c
Denis Rainer <denis.rainer@gmail.com>
2014-10-25 00:13:13 -0300

    Configuração de hibernate.

---
+++ Sistema-Vendas/denis.rainer/com/denis/raner/sistema/vendas/Conecta.java
@@ -0,0 +1,18 @@
+package com.denis.raner.sistema.vendas;
+import org.hibernate.Session;
+
+public class Conecta {
+	
+	public static void main(String[] args) {
+		Session session = null;
+		
+		try {
+			
+			session = HibernateUtil.getSession().openSession();
+			System.out.println("Conectou!!");
+		} finally{
+			session.close();
+			System.out.println("Desconectou!!");
+		}
+	}
+}
